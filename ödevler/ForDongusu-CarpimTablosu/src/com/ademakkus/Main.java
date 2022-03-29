b4985f45e6af3f68e48e3f67b671aa5fc754991b
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2022-03-29 06:17:32 +0300

    Add files via upload

---
+++ ödevler/ForDongusu-CarpimTablosu/src/com/ademakkus/Main.java
@@ -0,0 +1,13 @@
+package com.ademakkus;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <11 ; i++) {
+            for (int j = 1; j <11 ; j++) {
+                System.out.println(i+"*"+j+"="+(i*j));
+            }
+            System.out.println();
+        }
+    }
+}
